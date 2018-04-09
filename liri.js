2e332e314843444496d7e80c85eb2a9ca2effa3a
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-04-09 12:42:51 -0400

    latest

--- liri.js
+++ liri.js
@@ -1,4 +1,10 @@
-require("dotenv").config(); // installed using: npm install dotenv
+const result = require("dotenv").config(); // installed using: npm install dotenv
+
+if (result.error) {
+  throw result.error;
+}
+// console.log(result.parsed);
+
 
 var keys = require("./keys.js"); // this loads the keys.js file - RESPONSE on console: "this is loaded"
 var request = require("request");
@@ -6,29 +12,34 @@ var Twitter = require("twitter");
 
 var client = new Twitter(keys.twitter);
 
-command = process.argv[2]; //like 'my-tweets'
-name = process.argv[3]; //mu user account name
+command = process.argv[2]; //for example: 'my-tweets'
+// console.log(command);
+name = process.argv[3]; //for example:  user account name 'theRealMWright'
 
-execute(command, name); // calls the execute function which starts the process
+
 
 function execute(command, name) {
-  //takes 2 arguments -- command name and username
-  
+  //takes 2 arguments -- command name ('my-tweets') and username/artist
+
   if (command === "my-tweets") {
-    getTwitter(name); //runs the Twitter functionality
+    getTwitter(name); //calls the Twitter function
   }
 }
-
+// console.log("hi");
 
 function getTwitter(name) {
-  var params = { screen_name: "ladygaga" };
-  client.get("statuses/user_timeline", params, function(
+  var params = { screen_name: "theRealMWright" };
+  client.get('favorites/list', params, function(
     error,
     tweets,
     response
   ) {
-    if (!error) {
-      console.log(tweets);
-    }
+    if (error) throw error;
+    console.log(tweets);
+    console.log(response);
   });
 }
+
+
+//THIS IS FUNCTION where it all starts
+execute(command, name); // calls the execute function which starts the process
